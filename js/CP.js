c20d54a680840ecead7e260bd817ec41de18fcab
Juan Sebastian Aldana <75735926+sebastianuninorte@users.noreply.github.com>
2020-12-13 18:45:45 -0500

    Add files via upload

---
+++ js/CP.js
@@ -0,0 +1,15 @@
+function validarSoloNumeros(){
+
+    let expresion= new RegExp("[0-9]") //Se usa para una variable pero solo se puede usar dentro de la función
+    
+    if(expresion.test(texto.value)){
+        alert("Solamente hay números")
+    }else{
+        alert("Hay una o más letras")
+    }
+}
+boton.addEventListener("click", validarSoloNumeros)
+
+function actualizar() {
+    alert("Se ha modificado la información del producto");
+}
