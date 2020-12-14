256da717e5172e186ae3373427fef012b9c95fea
Sindy  Gonzalez Barrera <sindygonzalezbarrera@MacBook-Air-de-Sindy.local>
2020-12-13 21:12:12 -0500

    .

--- js/CP.js
+++ js/CP.js
@@ -1,40 +1,19 @@
-
-function Salir(){
-    window.close();
-}
-
-Salir.addEventListener("click", Salir)
-
-
-var texto = document.getElementById("Ref");
-var texto = document.getElementById("Precio");
-var texto = document.getElementById("Cantidad");
+var texto = document.getElementById("precio");
 var boton = document.getElementById("Guardar");
 
-
-function validarSoloNumerosRef(){
-
-    let expresion= new RegExp("[0-9]") //Se usa para una variable pero solo se puede usar dentro de la función
-    
-    if(expresion.test(texto.value)){
-        alert("Referencia Correcta")
-    }else{
-        alert("Hay una o más letras en el campo referencia. Ingrese solo números")
-    }
-}
-
-boton.addEventListener("click", validarSoloNumerosRef)
-
-
-function validarSoloNumerosPrecio(){
+function validarSoloNumeros(){
 
     let expresion= new RegExp("[0-9]") //Se usa para una variable pero solo se puede usar dentro de la función
     
     if(expresion.test(texto.value)){
-        alert("Referencia Correcta")
+        alert("Precio Correcto")
     }else{
         alert("Hay una o más letras en el campo precio. Ingrese solo números")
     }
 }
+boton.addEventListener("click", validarSoloNumeros)
 
-boton.addEventListener("click", validarSoloNumerosPrecio)
+function Salir(){
+    window.close();
+}
+Salir.addEventListener("click", Salir)
